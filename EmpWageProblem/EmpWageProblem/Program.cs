6be0297a4a50e3b4dfc1ad574552507922dde1c0
bhushan mawal <bhushanmawal2@gmail.com>
2022-03-10 16:33:25 +0530

    [bhushan]-employee is present or absent

--- EmpWageProblem/EmpWageProblem/Program.cs
+++ EmpWageProblem/EmpWageProblem/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("welcome to empwage compitation");
+
+    using System;
+int Fulltime = 1;
+Random random_num = new Random();
+int checkemp = random_num.Next(0, 2);
+if (checkemp == Fulltime)
+{
+    Console.WriteLine("Employee Is Present");
+}
+else
+{
+    Console.Write("Employee is Absent");
+}
